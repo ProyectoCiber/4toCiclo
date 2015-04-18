657293cfb44fb4a6408cdc8e62d4ca34b7b2f60b
zeler benji <benji_zeler@hotmail.com>
2015-04-18 00:10:57 -0500

    Eh Comentado estos

--- src/Controlador/Coneccion.java
+++ src/Controlador/Coneccion.java
@@ -3,7 +3,10 @@ package Controlador;
 public class Coneccion {
 	
 	public static void main(String[] args) {
-		System.out.println("Hola Mundillo");
+//		System.out.println("Hola Mundillo");
+//		System.out.println("asdsdsasdsd");
+		
+		
 	}
 
 }
